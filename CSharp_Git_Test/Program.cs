605d95b0e261870a95cf8290a19fadbbf990ac03
ThomasEnniss <teenniss@utas.edu.au>
2018-04-30 09:03:47 +1000

    Add project files.

---
+++ CSharp_Git_Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Git_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test_number = 1;
+            Console.WriteLine("Hello World! + {0}", test_number);
+            Console.ReadKey();
+        }
+    }
+}
